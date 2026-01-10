d05c72410682fc7c6554641009d7e326a88e1924
j <joinjerrodtoday@gmail.com>
2026-01-09 22:29:55 -0800

    update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-response = input("Enter to do: ")
+user_prompt = "Enter a todo:"
+todo1 = input(user_prompt)
+todo2 = input(user_prompt)
+todo3 = input(user_prompt)
 
-print(response)
+todos = [todo1, todo2, todo3]
+print(todos)
+print(type(todos))

bb6c082fce50557fc48c57546cea95ea3899be34
j <joinjerrodtoday@gmail.com>
2026-01-19 20:38:46 -0800

    Added match and cases

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-user_prompt = "Enter a todo:"
 
 todos = []
 
 while True:
-    todo = input(user_prompt)
-    print(todo.title())
-    todos.append(todo)
-    print(todos)
+    todo = input("Type add or show: ")
+    
+    match user_action:
+        case 'add':
+            todo = input("Enter a todo: ")
+            todos.append(todo)
+        case 'show':
+            print(todos)

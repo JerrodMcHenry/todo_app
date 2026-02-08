45f28ef91a57acf5a5189d65b9a87f25ebe19630
j <joinjerrodtoday@gmail.com>
2026-02-08 09:12:51 -0800

    changed file path

--- main.py
+++ main.py
@@ -7,20 +7,20 @@ while True:
         case 'add':
             todo = input("Enter a todo: ") + "\n"
 
-            file = open('todos.txt', 'r')
+            file = open('/todos.txt', 'r')
             todos = file.readlines()
             file.close()
 
             todos.append(todo)
 
-            file = open('todos.txt', 'w')
+            file = open('files/todos.txt', 'w')
             file.writelines(todos)
             file.close()
         case 'show':
-            file = open('todos.txt', 'r')
+            file = open('files/todos.txt', 'r')
             todos = file.readlines()
             file.close()
-            
+
             for index, item in enumerate(todos):
                 print(f"{index + 1}-{item}".title())
         case 'edit':

0cdee4dc475ef1cc60b9629b87cb4021c341b034
j <joinjerrodtoday@gmail.com>
2026-02-23 20:41:05 -0800

    Changed match cases to if statements

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ while True:
     user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
     
-    match user_action:
-        case 'add':
+    if 'add' in user_action:
             todo = input("Enter a todo: ") + "\n"
 
             with open('files/subfiles/todos.txt', 'r') as file:
@@ -16,7 +15,7 @@ while True:
             with open('files/subfiles/todos.txt', 'w') as file:
                 file.writelines(todos)
 
-        case 'show':
+    if 'show' in user_action:
             with open('files/subfiles/todos.txt', 'r') as file:
                 todos = file.readlines()
             
@@ -25,39 +24,39 @@ while True:
             for index, item in enumerate(todos):
                 item = item.strip("\n")
                 print(f"{index + 1}-{item}".title())
-        case 'edit':
-            number = int(input("Number of the todo to edit: "))
-            number = number - 1
+    if 'edit' in user_action:
+        number = int(input("Number of the todo to edit: "))
+        number = number - 1
 
-            with open('files/subfiles/todos.txt', 'r') as file:
-                todos = file.readlines()
-            print("Here is existing", todos)
+        with open('files/subfiles/todos.txt', 'r') as file:
+            todos = file.readlines()
+        print("Here is existing", todos)
 
-            new_todo = input("Enter new todo: ")
-            todos[number] = new_todo + '\n'
+        new_todo = input("Enter new todo: ")
+        todos[number] = new_todo + '\n'
 
-            print("Here is the added todo", todos)
+        print("Here is the added todo", todos)
 
-            with open('files/subfiles/todos.txt', 'w') as file:
-                todos = file.writelines(todos)
+        with open('files/subfiles/todos.txt', 'w') as file:
+            todos = file.writelines(todos)
+    
+    if 'complete' in user_action:
+        number = int(input("Number of the todo to complete: "))
+
+        with open('files/subfiles/todos.txt', 'r') as file:
+            todos = file.readlines()
         
-        case 'complete':
-            number = int(input("Number of the todo to complete: "))
-
-            with open('files/subfiles/todos.txt', 'r') as file:
-                todos = file.readlines()
-            
-            index = number - 1
-            todo_to_remove = todos[index].strip('\n')
-            todos.pop(index)
-            
-            with open('files/subfiles/todos.txt', 'w') as file:
-                todos = file.writelines(todos)
-
-            message = f"Todo {todo_to_remove} was removed from the list."
-            print(message)
-        case 'exit':
-            break
+        index = number - 1
+        todo_to_remove = todos[index].strip('\n')
+        todos.pop(index)
         
+        with open('files/subfiles/todos.txt', 'w') as file:
+            todos = file.writelines(todos)
+
+        message = f"Todo {todo_to_remove} was removed from the list."
+        print(message)
+    if 'exit' in user_action:
+        break
+    
 
 print("Bye!")
